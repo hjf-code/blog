78c545b6d406c57419be884dae835b78733680ce
黄建峰 <372832391@qq.com>
2023-03-02 19:19:06 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-config-provider namespace="ep">
+    <BaseHeader />
+    <div style="display: flex">
+      <BaseSide />
+      <div>
+        黄建峰的学习笔记
+        <div
+          style="
+            height: 30px;
+            width: 100%;
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          "
+        >
+          <img :src="police" />
+          <a
+            href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=43100302000292"
+            target="_blank"
+          >
+            &nbsp;湘公网安备 43100302000292号
+          </a>
+          <span>&nbsp;|&nbsp;</span>
+          <a href="https://beian.miit.gov.cn/" target="_blank">
+            湘ICP备2023002259号-1
+          </a>
+        </div>
+      </div>
+    </div>
+  </el-config-provider>
+</template>
+
+<script lang="ts" setup>
+  import police from '~/assets/police.png'
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: var(--ep-text-color-primary);
+}
+
+.element-plus-logo {
+  width: 50%;
+}
+</style>
---
+++ src/components/layouts/BaseHeader.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { toggleDark } from "~/composables";
+</script>
+
+<template>
+  <button
+    class="border-none w-full bg-transparent cursor-pointer"
+    @click="toggleDark()"
+    style="height: var(--ep-menu-item-height)"
+  >
+    <i inline-flex i="dark:ep-moon ep-sunny" />
+  </button>
+</template>
